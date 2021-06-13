f5b754769c2f0771313afd4563e4d97e84212def
R06ert <dominguezbert@gmail.com>
2021-06-13 23:19:15 +0200

    Logo changes

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,31 +1,39 @@
 import React from 'react';
-import './Menu.css'
+import './Menu.css';
 
 //component
 class Menu extends React.Component {
-    render() {
-      return (
-        <div className="Menu">
-          <img src="https://farmersbrothers.webnode.cz/_files/200000017-13f6213f64/200/TransparentLogo.png"></img>
-          <p>→ shit happens ←</p>
-          <navigator>
-            <div className="domů">DOMŮ</div>
-            <div className="ceník">CENÍK</div>
-            <div className="galerie">GALERIE</div>
-            <div className="kontakt">KONTAKT</div>
-          </navigator>
-          <footer>
-               <div className="instagram">
-              <img src="C:\Users\simon\Desktop\TransparentLogo"></img>
-              <p>farmersbrothers</p>
-              </div>
-              <div className="phone">
-              <p>+420 777 777 777</p>
-              </div>
-          </footer>
+  render() {
+    return (
+      <div className="Menu">
+        <div style={{
+          margin: '10% auto',
+          height: '17.5%',
+          backgroundImage: `url('../../../Assets/BIG_LOGO.png')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'contain',
+          backgroundPosition: 'center'
+        }}>
         </div>
-      )
-    }
+        <p>→ shit happens ←</p>
+        <navigator>
+          <div className="domů">DOMŮ</div>
+          <div className="ceník">CENÍK</div>
+          <div className="galerie">GALERIE</div>
+          <div className="kontakt">KONTAKT</div>
+        </navigator>
+        <footer>
+          <div className="instagram">
+            <img src="C:\Users\simon\Desktop\TransparentLogo"></img>
+            <p>farmersbrothers</p>
+          </div>
+          <div className="phone">
+            <p>+420 777 777 777</p>
+          </div>
+        </footer>
+      </div>
+    )
   }
+}
 
 export default Menu;
