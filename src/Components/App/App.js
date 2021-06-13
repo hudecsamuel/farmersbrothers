b3392c997475f992bf36d8c2d725e9acc886368e
R06ert <dominguezbert@gmail.com>
2021-06-13 19:27:19 +0200

    Test

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MainContent from '../MainContent/MainContent';
-import Menu from './Menu/Menu.js';
+import Menu from '../Menu/Menu';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Ahoj Šimone</h1>
+        <Menu />
         <MainContent />
       </div>
     )
