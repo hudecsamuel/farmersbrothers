3334ae98325a3890765fdd3677c0eded3908c370
R06ert <dominguezbert@gmail.com>
2021-06-13 19:17:50 +0200

    Test

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import MainContent from '../MainContent/MainContent';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Ahoj Šimone</h1>
+      <div>
+        <h1>Ahoj Šimone</h1>
+        <MainContent />
+      </div>
     )
   }
 }
