39157d6e797450c80eced59d2d4147c8b259cacc
R06ert <dominguezbert@gmail.com>
2021-06-15 07:28:11 +0200

    Reorganized Assets folder for future work on Gallery module...

--- src/Components/MainContent/Home/Home.js
+++ src/Components/MainContent/Home/Home.js
@@ -1,17 +1,10 @@
 import React from 'react';
+import './Home.css';
 
 class Home extends React.Component {
     render() {
         return (
-            <div style={{
-                width: '110%',
-                height: '110%',
-                backgroundImage: `url('../../../Assets/field_truck.jpg')`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center'
-            }}>
-            </div>
+            <div className="BG-image"></div>
         )
     }
 }
