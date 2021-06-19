92124ab0ad90fef1c4c88038c0e87883b4537b02
Simon <simonpeck97@gmail.com>
2021-06-19 18:51:58 +0200

    Závěr notes

--- src/Components/MainContent/Price/Price.js
+++ src/Components/MainContent/Price/Price.js
@@ -5,10 +5,18 @@ class Price extends React.Component {
     render() {
         return (
             <div>
-            <div className="Cover-photo"></div>
-            <div className="under-Image">
-            <p>CENÍK</p>
-            </div>
+                <div className="Cover-photo"></div>
+                <div className="under-Image">
+                    <h1 className="Contact-heading">CENÍK</h1>
+                    <div className="Main-div"></div>
+                
+
+                <div className="Notes"> 
+                    <span>→ vedené ceny jsou bez DPH</span>
+                    <span>→ možnost zajištění dopravy 24/7</span>
+                    <span>→ výše uvedené ceny nejsou závaznou nabídkou, ta bude zaslána na základě poptávky</span>
+                </div>
+                </div>
             </div>
         )
     }
