6e04456d8589db1939731c26af95df1030fd6f44
Simon <simonpeck97@gmail.com>
2021-06-22 23:08:02 +0200

    Hotovo switchování kalkulačky

---
+++ src/Components/MainContent/Price/Chosen/Cisterna/Cisterna.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Cisterna() {
+       
+    
+
+        return (
+           "hello"
+        )
+    }
+export default Cisterna;
--- src/Components/MainContent/Price/Price.js
+++ src/Components/MainContent/Price/Price.js
@@ -1,8 +1,50 @@
 import React,{useState} from 'react'
 import './Price.css'
+import Cisterna from './Chosen/Cisterna/Cisterna'
+import Sklizeň from './Chosen/Sklizeň/Sklizeň'
+import Zemní from './Chosen/Zemní/Zemní'
 
 function Price() {
-    const [show,setShow]=useState(true)
+    const [show,setShow]=useState(false)
+    const [look, setLook]=useState(true)
+    const [choice, setChoice]=useState(1)
+/*     constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+      } */
+
+     function handleClick(e) { /* když pracuji ve funkci, tak musíš napsat že se jedná o funkci, jinak to nepozná - debil */
+        const elText = e.target.innerText;
+        console.log(elText);
+        switch (elText) {
+          case 'CISTERNOVÁ DOPRAVA':
+            setChoice(1);
+            break; 
+          case 'SKLIZŇOVÁ DOPRAVA':
+            setChoice(2);
+            break;
+          case 'ZEMNÍ PRÁCE':
+            setChoice(3);
+            break;
+          default:
+            setChoice(1);
+        }
+        setShow(!show);
+      }
+
+      function showChoice(){
+        switch (choice) {
+            case 1:
+              return <Cisterna/>; 
+            case 2:
+              return <Sklizeň/>;
+            case 3:
+              return <Zemní/>;
+            default:
+              return <Cisterna/>;
+          }
+      }
+    
         return (
             <div>
                 <div className="Cover-photo-price"></div>
@@ -11,22 +53,25 @@ function Price() {
                     <div className="Main-div">
                         <div className="left">
                             <div className="calc">
-                                <div className="výběr">
-                                    <div className="roleta"><span>CISTERNOVÁ DOPRAVA</span> 
-                                    {    
-                                        show?<div id="dd">
-                                            <div>CISTERNOVÁ DOPRAVA</div>
-                                            <div>SKLIZŇOVÁ DOPRAVA</div>
-                                            <div>ZEMNÍ PRÁCE</div>
-                                        </div>:null
-                                    }
-                                        <div className="arrow" onClick={()=>setShow(!show)}></div>
+                                <div className="Chosen">
+                                    {showChoice()}
+                                    <div className="výběr">
+                                        <div className="roleta"><span>CISTERNOVÁ DOPRAVA</span> 
+                                        {    
+                                            show?<div id="dd">
+                                                <div onClick={handleClick}>CISTERNOVÁ DOPRAVA</div>
+                                                <div onClick={handleClick}>SKLIZŇOVÁ DOPRAVA</div>
+                                                <div onClick={handleClick}>ZEMNÍ PRÁCE</div>
+                                            </div>:null
+                                        }
+                                            <div className="arrow" onClick={()=>setShow(!show)}></div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div className="result">
-                                <span>Předpokládaná cena:</span>
-                                <span>2850 Kč</span>
+                                <span onClick={()=>setLook(!look)}>Předpokládaná cena:</span>
+                                { look?<span>2850 Kč</span>:null}
                             </div>
                         </div>
                         <div className="right"><p>Obvyklá míra napuštění cisterny je 21 m<sup>3</sup></p></div>
@@ -42,6 +87,7 @@ function Price() {
             </div>
         );
 
+        
           /*   let clicks = false;
             
              let dropdown = function(){
